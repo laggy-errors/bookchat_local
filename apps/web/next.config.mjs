6b325aa51ee5dd5c05f3e6a77575a2afb8bd6e02
laggy-errors <rachhera@gmail.com>
2026-07-17 23:25:25 +0530

    added env part 2

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -2,21 +2,22 @@
 const nextConfig = {
   async rewrites() {
     return [
-      {
-        source: '/auth/google',
-        destination: 'http://localhost:3001/api/auth/google',
-      },
-      {
-        source: '/auth/google/callback',
-        destination: 'http://localhost:3001/api/auth/google/callback',
-      },
+      // {
+      //   source: '/auth/google',
+      //   destination: 'http://localhost:3001/api/auth/google',
+      // },
+      // {
+      //   source: '/auth/google/callback',
+      //   destination: 'http://localhost:3001/api/auth/google/callback',
+      // },
       {
         source: '/api/:path*',
-        destination: 'http://localhost:3001/api/:path*',
+        // destination: 'http://localhost:3001/api/:path*',
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/api/:path*`
       },
       {
-        source: '/socket.io/:path*',
-        destination: 'http://localhost:3001/socket.io/:path*',
+        // source: '/socket.io/:path*',
+        // destination: 'http://localhost:3001/socket.io/:path*',
       },
     ];
   },
